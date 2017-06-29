6039d5923364fb0b6256c2abc586dbb8432afb7c
Bart Reedijk <bart@mrdk.nl>
2017-06-29 09:19:14 +0200

    working weather forecast

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { HttpModule} from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { WeatherService } from './weather.service';
@@ -15,7 +16,8 @@ import {NgxChartsModule} from '@swimlane/ngx-charts';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    NgxChartsModule
+    NgxChartsModule,
+    HttpModule
   ],
   providers: [WeatherService],
   bootstrap: [AppComponent]
--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class WeatherService {
 
-  constructor() { }
+  private URL = 'http://api.openweathermap.org/data/2.5/forecast?q=Breda,NL&units=metric&lang=nl&APPID=cf977452681e43b2caca170a492b4657';
+
+  constructor(private http: Http) { }
+
+  doRequest(): Observable<any> {
+    return this.http.get(this.URL)
+      .map(this.extractData)
+      .catch(this.handleError);
+  }
+
+  private extractData(res: Response) {
+    let body: any;
+    body = res.json();
+
+    console.log('response:');
+    console.log(body);
+
+    if (body.cod !== '200') {
+      console.log('---------------------------');
+      this.handleError(Response);
+    }
+
+    return body || { };
+  }
+
+  private handleError (error: Response | any) {
+    // In a real world app, you might use a remote logging infrastructure
+    let errMsg: string;
+    if (error instanceof Response) {
+      const body = error.json() || '';
+      const err = body.error || JSON.stringify(body);
+      errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+    } else {
+      errMsg = error.message ? error.message : error.toString();
+    }
+    console.error(errMsg);
+    return Observable.throw(errMsg);
+  }
 
 }
+
